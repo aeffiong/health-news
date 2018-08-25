c47fbd30aaad23fbaec6dcf33ec99afd3bfaa16a
aeffiong <aeffiong9@gmail.com>
2018-08-24 19:03:09 -0600

    changing from request npm package to axios npm package to get articles to show up on /articles route.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-// Require request and cheerio. This makes the scraping possible
-var request = require("request");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
 var cheerio = require("cheerio");
 
 var PORT = 8080;
@@ -42,23 +42,23 @@ app.get("/", function (req, res) {
 // A GET route for scraping the Greatist website
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with request
-    request("https://greatist.com/", function(error, response, html) {
+    axios.get("https://greatist.com/").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(html);
+      var $ = cheerio.load(response.data);
         // grab all the class = desc from the html
         $("div.desc").each(function(i, element) {
             // An empty object to save the data that we'll scrape
-            var results = {};
+            var result = {};
 
-            results.link = $(this).children().children().attr("href");
-            results.title = $(this).children().children().html(); 
-            results.description = $(this).find($(".blurb-cta-wrapper")).text();
+            result.title = $(this).children().children().html(); 
+            result.link = $(this).children().children().attr("href");
+            result.description = $(this).find($(".blurb-cta-wrapper")).text();
 
              // Log the results once you've looped through each of the elements found with cheerio
-            console.log(results);
+            console.log(result);
          
         // Create a new Article using the `results` object built from scraping
-        db.Article.create(results)
+        db.Article.create(result)
           .then(function(dbArticle) {
             // View the added result in the console
             console.log(dbArticle);
@@ -76,14 +76,13 @@ app.get("/scrape", function(req, res) {
 
 // Route for getting all Articles from the db - not currently working
 app.get("/articles", function(req, res) {
-    db.Article.find({}), function(err, articles) {
-        if (err) {
-            return err;
-            console.log(err);
-        } else {
-            res.json(articles);
-        }
-    }
+    db.Article.find({})
+     .then(function(dbArticle) {
+         res.json(dbArticle);
+     })
+     .catch(function(err) {
+         res.json(err);
+     });
   });
   
   // Route for grabbing a specific Article by id, populate it with it's note
