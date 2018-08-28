906734990df70bb544c7a0cf2c7eb00c2f7e0b3b
aeffiong <aeffiong9@gmail.com>
2018-08-27 21:40:31 -0600

    adding info for deploying to Heroku

--- server2.js
+++ server2.js
@@ -27,7 +27,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/greatistscraper");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/greatistscraper"
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
